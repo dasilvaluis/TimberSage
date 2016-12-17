55041ac63d64e6996b49547e409543ccbd14232b
Luís Silva <luismiguel.s91@gmail.com>
2016-12-17 21:02:16 +0000

    process single template like page template

--- single.php
+++ single.php
@@ -1,1 +1,4 @@
-<?php get_template_part('templates/content-single', get_post_type()); ?>
+<?php while (have_posts()) : the_post(); ?>
+    <?php get_template_part('templates/header', 'single'); ?>
+    <?php get_template_part('templates/content-single', get_post_type()); ?>
+<?php endwhile; ?>
