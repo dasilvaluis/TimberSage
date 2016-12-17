dbc481a5bd0de3035dc735ebd8385e68153c0a90
Luís Silva <luismiguel.s91@gmail.com>
2016-12-17 20:34:14 +0000

    trade jshint for eslint

--- gulp-tasks/scripts.js
+++ gulp-tasks/scripts.js
@@ -1,7 +1,7 @@
 var uglify      = require('gulp-uglify');
 var sourcemaps  = require('gulp-sourcemaps');
-var jshint      = require('gulp-jshint');
 var concat      = require('gulp-concat');
+var eslint      = require('gulp-eslint');
 
 // ### JS processing pipeline
 // Example
@@ -34,19 +34,26 @@ var jsTasks = function(filename) {
 };
 
 // ### Scripts
-// `gulp scripts` - Runs JSHint then compiles, combines, and optimizes Bower JS
+// `gulp scripts` - Runs ESLint then compiles, combines, and optimizes Bower JS
 // and project JS.
-gulp.task('scripts', ['jshint'], function() {
+gulp.task('scripts', ['eslint'], function() {
   return gulp.src(deps.js)
     .pipe(jsTasks('main.js'))
     .pipe(writeToManifest('scripts'));
 });
 
-// ### JSHint
-// `gulp jshint` - Lints configuration JSON and project JS.
-gulp.task('jshint', function() {
+// ### ESLint
+// `gulp eslint` - Lints configuration JSON and project JS.
+// Configure linter options in eslint.json
+gulp.task('eslint', function() { 
   return gulp.src(['../gulpfile.js', '../config.json'].concat(config.jslintFiles))
-    .pipe(jshint())
-    .pipe(jshint.reporter('jshint-stylish'))
-    .pipe(gulpif(enabled.failJSHint, jshint.reporter('fail')));
+    // eslint() attaches the lint output to the "eslint" property 
+    // of the file object so it can be used by other modules. 
+    .pipe(eslint({configFile: 'eslint.json'}))
+    // eslint.format() outputs the lint results to the console. 
+    // Alternatively use eslint.formatEach() (see Docs). 
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on 
+    // lint error, return the stream and pipe to failAfterError last. 
+    .pipe(gulpif(enabled.failJSHint, eslint.failAfterError()));
 });
