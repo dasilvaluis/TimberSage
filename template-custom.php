b82885881b255574e7635e6f25ba1a6d8deb2b66
Luís Silva <luismiguel.s91@gmail.com>
2017-02-26 00:01:21 +0000

    Remove unnecessary files

--- template-custom.php
+++ template-custom.php
@@ -2,9 +2,5 @@
 /**
  * Template Name: Custom Template
  */
-?>
 
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/page', 'header'); ?>
-  <?php get_template_part('templates/content', 'page'); ?>
-<?php endwhile; ?>
+get_template_part('page');
